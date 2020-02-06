b40f6cf05b1a8fd39bd62f55913ffbb4a795435d
BenoitAudigier <32508114+BenoitAudigier@users.noreply.github.com>
2020-02-06 11:37:39 +0100

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# From Wikipedia: Torvalds has quipped about the name git, which is British English slang for a stupid or unpleasant person. 
+# Torvalds said: "I'm an egotistical bastard, and I name all my projects after myself. First 'Linux', now 'git'.The man page 
+# describes git as "the stupid content tracker".
+# 
+# D'où la traduction approximative ci-dessous:
+New-Alias fdp git
