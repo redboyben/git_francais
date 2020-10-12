22ffed13260af34b138808e9f4f37e15dd1b882f
BenoitAudigier <32508114+BenoitAudigier@users.noreply.github.com>
2020-10-12 15:15:50 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,13 @@
 New-Alias con git
+
+######## Prompt color, not necessary ########
+# requires posh-git
+Import-Module posh-git
+function prompt {
+    # Your non-prompt logic here
+    $ESC = [char]27
+    " $ESC[46m$(Get-Date -Format "HH:mm")$ESC[0m >"
+    # Have posh-git display its default prompt
+    & $GitPromptScriptBlock
+    $GitPromptSettings.EnableFileStatus = $false
+}
